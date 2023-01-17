b628ec7014bee7dfa5a702828b31ad43d4e95079
Siril Ganjai <g.saisiril@gmail.com>
2023-01-17 17:25:57 -0500

    Linked List in C.
    It links the data 0-9 in a linked list, and prints the data
    on the console in the same way as it was enterd.
    
    Liked list as a concept is easy. Implementing the same in C
    required understanding of memory allocation and pointers.
    I'd recommend reading the use of struct and pointers, and a decent
    understanding of linkedlists before getting into the code.
    
    commit 2
    
    commit 3
    
    commit 4

---
+++ linked_list.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+The whole idea of linkedlist is to have a pointer to the next node.
+This is a very simple implementation of linkedlist. 
+
+BUT WHY CARE !? :/
+
+Ever thought how spotify or apple music works? One of the data structure used is linkedlist.
+
+The next, shuffle, and playlists are all stored in linkedlist. Infact, I couldnt think of 
+any better data structure. Ofcourse, you could argue that graphs are better. But, graphs, trees 
+are all essentially linkedlists. Its imprtant to understand linkedlist on a memory level, and 
+use it in our projects when ever requreid. 
+
+This could be a part of system design interview.
+*/
+
+
+
+
+
+struct Node 
+{
+    int a;
+    struct Node *next;
+};
+int main()
+{ 
+    /**
+    Here we create 2 objects to iterate through a linkedlist.
+    We use pointer to iterate over linkedlist. Not the data itself. 
+    Inorder to go to a particular location, we need to have that location first
+    That is why, we use malloc. malloc is not encouraged in embedded systems in general.
+    This is just to demonstrate linkedlist. 
+
+    
+    */
+    struct Node obj;
+    struct Node *ptr;
+    
+    ptr = &obj;
+    for(int i = 0;i<9;i++)
+    {
+        // create a new memory and point the current's next to the new memory
+        ptr->next = (struct Node*)malloc(sizeof(struct Node));
+        ptr->a = i;
+        // update the current pointer to the curr->next, so next time it will update
+        // the next data in the linkedlist.
+        ptr=ptr->next; 
+
+    }
+
+    ptr->a = 9;
+    struct Node *head = &obj;
+    // until head == 0, loop throgh it. and once its zero, it comes out.
+    while(head!=0)
+    {
+        printf("%d \n",*(head));
+        head = head->next;
+    }
+    
+    return 0;
+}
